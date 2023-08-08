d3c84f1c73f94b87bc20caa5487e113acebb18b4
LuiTheSkeleton <gigela19@freeuni.edu.ge>
2023-08-08 23:15:49 +0400

    Login Servlet first draft

---
+++ QuizProject/src/main/java/servlets/LogIn.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LogIn extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    public static final String COOKIE_NAME = "userCookie";
+
+    public LogIn() {}
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher RD = request.getRequestDispatcher("homepage.jsp");
+        RD.forward(request, response);
+    }
+}
