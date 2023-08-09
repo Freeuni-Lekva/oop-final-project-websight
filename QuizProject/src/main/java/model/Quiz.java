1e9e89595d61ebf75a72b8086597f1a0aefb137c
tgagn19 <tgagn19@freeuni.edu.ge>
2023-08-09 21:24:56 +0400

    small change

--- QuizProject/src/main/java/model/Quiz.java
+++ QuizProject/src/main/java/model/Quiz.java
@@ -1,4 +1,8 @@
 package model;
 
 public class Quiz {
+    private static final int QUESTION_RESPONSE = 1;
+    private static final int FILL_BLANK = 2;
+    private static final int MULTIPLE_CHOICE = 3;
+    private static final int PICTURE_RESPONSE = 4;
 }
