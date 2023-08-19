320d367e9d89f892fc3aa67f951c0dc67f5e91ad
davitpatsina <dpats19@freeuni.edu.ge>
2023-08-19 17:44:54 +0400

    AnswerTests

---
+++ QuizProject/src/main/java/Tests/QuestionTest.java
@@ -0,0 +1,73 @@
+package model;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuestionTest extends TestCase {
+
+    @Test
+    public void addCorrectAnswer() {
+    }
+
+    @Test
+    public void showQuestionText() {
+    }
+
+    @Test
+    public void getQuestionID() {
+    }
+
+    @Test
+    public void setQuestionID() {
+    }
+
+    @Test
+    public void getQuestionType() {
+    }
+
+    @Test
+    public void setQuestionType() {
+    }
+
+    @Test
+    public void getQuizID() {
+    }
+
+    @Test
+    public void setQuizID() {
+    }
+
+    @Test
+    public void getQuestionText() {
+    }
+
+    @Test
+    public void setQuestionText() {
+    }
+
+    @Test
+    public void getProbableAnswers() {
+    }
+
+    @Test
+    public void setProbableAnswers() {
+    }
+
+    @Test
+    public void getCorrectAnswer() {
+    }
+
+    @Test
+    public void setCorrectAnswer() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+}
