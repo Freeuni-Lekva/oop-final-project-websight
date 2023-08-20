d7458a61e72b2d5aee5fa1be72f66d4a7dff59ae
davitpatsina <dpats19@freeuni.edu.ge>
2023-08-20 14:54:48 +0400

    Add new version of Question

--- QuizProject/src/main/java/model/Question/FBQuestion.java
+++ QuizProject/src/main/java/model/Question/FBQuestion.java
@@ -1,2 +1,18 @@
-package model.Question;public class FBQuestion {
+package model.Question;
+
+import model.Answer.FBAnswer;
+import model.DBConnection;
+
+import java.sql.SQLException;
+
+public class FBQuestion extends Question{
+    public FBQuestion(int questionId, String questionText, int questionType, int questionNum, int quizId, DBConnection db) throws SQLException {
+        super(questionId, questionText, questionType, questionNum, quizId, db);
+    }
+
+    @Override
+    public void addAnswers(DBConnection db)
+            throws NumberFormatException, SQLException {
+        answers = new FBAnswer(questionID, db);
+    }
 }
--- QuizProject/src/main/java/model/Question/PRQuestion.java
+++ QuizProject/src/main/java/model/Question/PRQuestion.java
@@ -1,2 +1,33 @@
-package model.Question;public class PRQuestion {
+package model.Question;
+
+import model.Answer.PRAnswer;
+import model.DBConnection;
+
+import java.sql.SQLException;
+
+public class PRQuestion extends Question{
+    public PRQuestion(int questionID, String questionText, int questionType, int questionNum, int quizID, DBConnection db) throws SQLException {
+        super(questionID, questionText, questionType, questionNum, quizID, db);
+    }
+
+    @Override
+    public void addAnswers(DBConnection db)
+            throws NumberFormatException, SQLException {
+        answers = new PRAnswer(questionID, db);
+    }
+
+    @Override
+    public String showQuestionWithAnswers() {
+        String html = "<p>Identify the picture:</p>";
+        html += ("<p><img src='" + questionText + "' alt='Picture not found' width='200'/></p>");
+        html += answers.getAnswersHTML();
+        return html;
+    }
+
+    @Override
+    public String showQuestionText() {
+        String html = "<p>Identify the picture:</p>";
+        html += ("<p><img src='" + questionText + "' alt='Picture not found' width='200'/></p>");
+        return html;
+    }
 }
